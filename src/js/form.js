b87e5bd27c530831f567a7166cb2ac77de2b9a54
Peter Douglas <peterdouglas08@gmail.com>
2023-09-25 12:54:23 -0300

    ajustando erros - parte 3

--- src/js/form.js
+++ src/js/form.js
@@ -1,6 +1,20 @@
 const dadosUsuario = document.querySelectorAll(".dados");
 const botaoForm = document.querySelector(".botao");
 
+dadosUsuario.forEach(input => {
+  input.addEventListener("input", () => {
+    if (input.value) {
+      input.classList.add("aceito");
+      input.classList.remove("recusado");
+      input.nextElementSibling.classList.add("alertaOff");
+    } else {
+      input.classList.remove("aceito");
+      input.classList.add("recusado");
+      input.nextElementSibling.classList.remove("alertaOff");
+    }
+  });
+});
+
 botaoForm.addEventListener("click", (event) => {
   event.preventDefault();
 
